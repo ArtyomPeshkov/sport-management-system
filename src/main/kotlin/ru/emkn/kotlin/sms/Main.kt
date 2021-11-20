d6a01ae8e8aa43c4a24da7285b94783fa5c4027c
Artem <artemyipeshkov@gmail.com>
2021-11-20 17:10:41 +0300

    logger setup

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -1,5 +1,16 @@
 package ru.emkn.kotlin.sms
 
+import mu.KotlinLogging
+
+val logger = KotlinLogging.logger { }
+
 fun main(args: Array<String>) {
-    TODO()
+    logger.info { "Hello world" }
+    logger.debug { "Something expensive to compute" }
+    try {
+        println(1/0)
+    } catch(exc: Exception) {
+        logger.error { exc.message }
+    }
+
 }
