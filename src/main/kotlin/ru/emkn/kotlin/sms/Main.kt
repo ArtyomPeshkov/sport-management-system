82a703bddb7cba6432c6f9e98c2c26c0cf60d157
Artem <artemyipeshkov@gmail.com>
2021-11-20 21:33:57 +0300

    final log settings

--- src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Main.kt
@@ -1,17 +1,5 @@
 package ru.emkn.kotlin.sms
-import log.*
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 
-
-val logger1: Logger = LoggerFactory.getLogger("asdad")
 fun main(args: Array<String>) {
-    logger1.infoC("Hello world")
-    logger1.debugC ("Something expensive to compute")
-    try {
-        println(1/0)
-    } catch(exc: Exception) {
-        logger1.error (exc.message)
-    }
 
 }
