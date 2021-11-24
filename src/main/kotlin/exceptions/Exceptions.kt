9bb75b6c60cd2c0635834091d3346eb8f4de278f
Artem <artemyipeshkov@gmail.com>
2021-11-25 00:51:16 +0300

    Time class + some Exceptions.kt modifications

--- src/main/kotlin/exceptions/Exceptions.kt
+++ src/main/kotlin/exceptions/Exceptions.kt
@@ -2,20 +2,26 @@ package exceptions
 
 import log.*
 
-class ProblemWithFilePath(path: String) :
-    Exception("Обнаружена проблема в этом пути:$YELLOW $path$RESET") {}
+class IllegalTimeFormatException(value: Any?) :
+    Exception("Ошибка в строковом представлении времени (необходимый формат hh:mm:ss):$YELLOW $value$RESET") {}
 
-class ProblemWithCSV(path: String) :
-    Exception("Проблема с количеством строк или форматом полей файла:$YELLOW $path$RESET") {}
+class UnexpectedValueException(value: Any?) :
+    Exception("Обнаружено неожиданное значение:$YELLOW $value$RESET")
+
+class ProblemWithFilePathException(path: String) :
+    Exception("Обнаружена проблема в этом пути:$YELLOW $path$RESET")
+
+class ProblemWithCSVException(path: String) :
+    Exception("Проблема с количеством строк или форматом полей файла:$YELLOW $path$RESET")
 
 class SexException(sex: String) :
-    Exception("Неправильно уложен пол!:$YELLOW $sex$RESET") {}
+    Exception("Неправильно уложен пол!:$YELLOW $sex$RESET")
 
-class CSVFileStringWithNameException(path: String) :
+class CSVStringWithNameException(path: String) :
     Exception("Неправильный формат первой строки (только первый столбец должен быть не пустым) в файле:$YELLOW $path$RESET")
 
-class CSVFileExceptionWithFieldNames(path: String) :
-    Exception("Неправильно указаны названия столбцов в файле:$YELLOW $path$RESET") {}
+class CSVFieldNamesException(path: String) :
+    Exception("Неправильно указаны названия столбцов в файле:$YELLOW $path$RESET")
 
 class CollectiveFileStringException(path: String, index: Int) :
-    Exception("Неправильный формат строки номер $index в файле:$YELLOW $path$RESET") {}
+    Exception("Неправильный формат строки номер $index в файле:$YELLOW $path$RESET")
