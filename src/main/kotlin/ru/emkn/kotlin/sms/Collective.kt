28566dba1e197b9fdd31febc0d76a04b36e8d479
Artem <artemyipeshkov@gmail.com>
2021-11-27 14:44:54 +0300

    Event is ready

--- src/main/kotlin/ru/emkn/kotlin/sms/Collective.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Collective.kt
@@ -1,10 +1,62 @@
 package ru.emkn.kotlin.sms
 
-class Collective(val name: String) {
-    private val athleteList: MutableList<Participant> = mutableListOf()
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
+import exceptions.CSVFieldNamesException
+import exceptions.CSVStringWithNameException
+import exceptions.IncorrectControlPointValue
+import exceptions.ProblemWithCSVException
+import log.debugC
+import java.io.File
+
+class Collective {
+    val name:String
+    private var athleteList: MutableList<Participant> = mutableListOf()
     var points = 0
     fun addParticipant(participant: Participant){
         athleteList.add(participant)
         points+=participant.points
+    }
+
+    fun makeParticipant(name:String, param: Map<String, String>, index: Int, path: String): Participant {
+        parseLogger.debugC("Reading participant number ${index + 1} from $path")
+        try {
+            val participant= Participant(
+                param["Группа"]?:throw CSVFieldNamesException(path),
+                chooseSex(param["Группа"]!![0].toString()),
+                param["Фамилия"]?:throw CSVFieldNamesException(path),
+                param["Имя"]?:throw CSVFieldNamesException(path),
+                param["Г.р."]?.toInt() ?:throw CSVFieldNamesException(path),
+                param["Разр."] ?: throw CSVFieldNamesException(path)
+            )
+            participant.setCollective(name)
+            return participant
+        } catch (e: Exception) {
+            throw CSVFieldNamesException(path)
+        }
+    }
+
+    fun participantsParser(name:String,file: File):List<Participant> {
+
+        if (file.readLines().size<3)
+            throw ProblemWithCSVException(file.path)
+        return             csvReader().readAllWithHeader(file.readLines().drop(1).joinToString("\n"))
+                .mapIndexed() { index, it -> makeParticipant(name, it, index, file.path) }
+    }
+
+    fun collectiveParser(path: String): List<Participant>{
+        parseLogger.debugC("Started reading participants from $path")
+        return  participantsParser(name,readFile(path))
+    }
+
+    constructor(path: String)
+    {
+        val file = readFile(path)
+        val fileStrings = csvReader().readAll(file.readText().substringBefore("\n"))
+        name = fileStrings[0].let {
+            if (it.size != 5 || it.drop(1).any { el -> el.isNotEmpty() })
+                throw CSVStringWithNameException(path)
+            else it[0]
+        }
+        athleteList = collectiveParser(path).toMutableList()
     }
 }
--- src/main/kotlin/ru/emkn/kotlin/sms/Event.kt
+++ src/main/kotlin/ru/emkn/kotlin/sms/Event.kt
@@ -10,11 +10,14 @@ import java.io.File
 import java.time.*
 
 
-class Event(path: String) {
+class Event {
     val name: String
     val date: LocalDate
     var groupList: List<Group> = listOf()//список групп
-    private var distanceList: List<Distance> = listOf()
+    private var distanceList: List<Distance> = listOf()//список дистанций
+    var collectiveList: List<Collective> = listOf()//список заявленных коллективов
+
+
     fun getDistanceByName(name: String): Distance? {
         return distanceList.find { it.name == name }
     }
@@ -24,7 +27,8 @@ class Event(path: String) {
         return groupStrings.map { group ->
             Group(
                 group["Название"] ?: throw CSVFieldNamesException(groups.path),
-                distanceList.find { dist -> dist.name == group["Дистанция"] } ?: throw CSVFieldNamesException(groups.path))
+                distanceList.find { dist -> dist.name == group["Дистанция"] }
+                    ?: throw CSVFieldNamesException(groups.path))
         }
     }
 
@@ -34,35 +38,40 @@ class Event(path: String) {
 
     }
 
-    fun eventParser(path: String): Pair<String, LocalDate> {
+
+    fun collectivesParser(applicationsFolder:File):List<Collective>
+    {
+        val applications = applicationsFolder.walk().toList().filter { ".*\\.csv".toRegex().matches(it.path.substringAfterLast('/')) }
+        return applications.map{ Collective(it.path) }
+    }
+
+
+    constructor (path: String) {
         parseLogger.debugC("Parsing event folder: $path")
         val configurationFolder = readFile(path).walk().toList()
         distanceList = distancesParser(configurationFolder.find { it.name.substringAfterLast('/') == "distances.csv" }
             ?: throw NotEnoughConfigurationFiles(path))
-        groupList = groupsParser(configurationFolder.find { it.name.substringAfterLast('/') == "groupsAndDistances.csv" }
+        groupList =
+            groupsParser(configurationFolder.find { it.name.substringAfterLast('/') == "groupsAndDistances.csv" }
+                ?: throw NotEnoughConfigurationFiles(path))
+        collectiveList =collectivesParser(configurationFolder.find { it.name.substringAfterLast('/') == "applications" }
             ?: throw NotEnoughConfigurationFiles(path))
 
-         val rows = csvReader().readAllWithHeader(configurationFolder.find { it.name.substringAfterLast('/') == "event.csv" }
-             ?: throw NotEnoughConfigurationFiles(path))
-         if (rows.size != 1)
-             throw ProblemWithCSVException(path)
-         else if (rows[0]["Дата"] != null && rows[0]["Название"] != null && rows[0].size == 2) {
-             return Pair(rows[0]["Название"] ?: "", LocalDate.parse(rows[0]["Дата"], formatter))
-         } else
-             throw CSVStringWithNameException(path)
+        //TODO("Вынести в отдельную функцию")
+        val rows =
+            csvReader().readAllWithHeader(configurationFolder.find { it.name.substringAfterLast('/') == "event.csv" }
+                ?: throw NotEnoughConfigurationFiles(path))
+        if (rows.size != 1)
+            throw ProblemWithCSVException(path)
+        else if (rows[0]["Дата"] != null && rows[0]["Название"] != null && rows[0].size == 2) {
+            name = rows[0]["Название"] ?: ""
+            date = LocalDate.parse(rows[0]["Дата"], formatter)
+        } else
+            throw CSVStringWithNameException(path)
     }
 
-
-    var collectiveList: MutableList<Collective> = mutableListOf()//список заявленных коллективов
-
-    init {
-        eventParser(path).let {
-            name = it.first
-            date = it.second
-        }
-    }
 
     override fun toString(): String {
-        return "Название:$name, дата: $date, количество групп: ${groupList.size}, количество дистанций: ${distanceList.size}"
+        return "Название:$name, дата: $date, количество групп: ${groupList.size}, количество дистанций: ${distanceList.size}, количество коллективов: ${collectiveList.size}"
     }
 }
